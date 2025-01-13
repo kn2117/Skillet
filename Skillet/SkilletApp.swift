0d2ee0d75e7fe66d54a512d3e7c3063bdab1ef43
kn2117 <89276194+kn2117@users.noreply.github.com>
2025-01-12 23:37:30 -0800

    Initial Commit

---
+++ Skillet/SkilletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkilletApp.swift
+//  Skillet
+//
+//  Created by Kevin Nakadate on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SkilletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
